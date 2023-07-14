e21c122c4dde765f1d1aedb7bb49e45f9c1298ee
unknown <sanchit2005yudavanshi@gmail.com>
2023-07-14 10:26:08 +0530

    creaed a search bar and added seach icon in it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <SearchBar/>
     </div>
   );
 }
---
+++ src/SearchBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+
+export default function SearchBar() {
+   return (
+        <Box
+            sx={{
+                width: 500,
+                maxWidth: '100%',
+                margin: "8em 10em 0em 25em"
+            }}
+        >
+
+            <TextField fullWidth label={<SearchIcon/>} id="fullWidth" autoFocus />
+        </Box>
+    );
+}
